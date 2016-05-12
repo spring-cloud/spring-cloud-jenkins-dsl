177fb6dbd7eff5f4553d70f94a15ae0f210c75cc
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-05-13 01:17:57 +0200

    Added multijob compatibility check

---
+++ jobs/springcloud/spring_cloud_temp.groovy
@@ -0,0 +1,70 @@
+package springcloud
+
+import io.springframework.cloud.compatibility.BootCompatibility
+import io.springframework.cloud.view.DashboardViewBuilder
+import javaposse.jobdsl.dsl.DslFactory
+
+DslFactory dsl = this
+
+new BootCompatibility(dsl).build()
+
+new DashboardViewBuilder(this).buildDashboard()
+
+dsl.listView('Seeds') {
+	jobs {
+		regex('.*-seed')
+	}
+	columns defaultColumns()
+}
+
+dsl.nestedView('Spring Cloud') {
+	views {
+		listView('Compatibility') {
+			jobs {
+				regex('spring-cloud.*-compatibility-check')
+			}
+			columns defaultColumns()
+		}
+		listView('CI') {
+			jobs {
+				regex('spring-cloud.*-ci')
+			}
+			columns defaultColumns()
+		}
+		listView('E2E') {
+			jobs {
+				regex('spring-cloud.*-e2e')
+			}
+			columns defaultColumns()
+		}
+		listView('Sonar') {
+			jobs {
+				regex('spring-cloud.*-sonar')
+			}
+			columns defaultColumns()
+		}
+		listView('F2F') {
+			jobs {
+				regex('spring-cloud.*-f2f')
+			}
+			columns defaultColumns()
+		}
+		listView('All Cloud') {
+			jobs {
+				regex('spring-cloud.*')
+			}
+			columns defaultColumns()
+		}
+	}
+}
+
+private Closure defaultColumns() {
+	return {
+		status()
+		name()
+		lastSuccess()
+		lastFailure()
+		lastBuildConsole()
+		buildButton()
+	}
+}
